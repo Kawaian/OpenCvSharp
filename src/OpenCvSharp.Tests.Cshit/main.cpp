e7889af9814292a0bdd6e94a15510aeaf10e094a
AinL <gmlwns5176@gmail.com>
2017-10-21 16:14:00 +0900

    add cpp tests

---
+++ src/OpenCvSharp.Tests.Cshit/main.cpp
@@ -0,0 +1,29 @@
+#include "main.h"
+using namespace std;
+
+#define SHIT
+#ifdef SHIT
+int reee = 0;
+#define MY() int main(
+#define LAIFU() ){
+#define ISFCKED() }
+#define CBLUSHBLUSH(body, count) for(long blush= 0; blush<count; blush++){body;};
+#define FUCK(str) cout << str;
+#define FUCKING() cout << '=';
+#define SUMFUK(str) for(long somenice = 0; somenice<10; somenice++) { str; };
+#define SUMFUK2(str, count) for(long smfu= 0; smfu<count; smfu++){str;};
+#define FCKED() cout << endl;
+#define NAH() cout << ' ';
+#define DAFUCK() FUCKING() FUCK("hi") SUMFUK(FUCKING()) FUCK("hi") FUCKING()
+#define DAFUCK2(count) FUCKING() FUCK("hi") SUMFUK2(FUCKING(), count) FUCK("hi") FUCKING()
+#define BRAIN(type, varname, content) type varname = content;
+#define COFFEE() cin.get();return 0;
+#define LELE2(type, varname, content) type varname##5 = content;type* varname##4 = &varname##5;type** varname##3 = &varname##4; type*** varname##2 = &varname##3; type**** varname##1 = &varname##2; type***** varname = &varname##1;
+#define ELEL2(name) (*****name)
+#define LELE(type, varname, content) LELE2(type, varname##TT, content) LELE2(type##*****, varname, varname##TT)
+#define ELEL(name) ELEL2(ELEL2(name))
+#endif
+
+MY() int a, char** args LAIFU() 
+LELE(long, MAX, 5000) LELE(long, NICE, 50) CBLUSHBLUSH(DAFUCK2(3) NAH() FUCK(blush) FUCK('/') FUCK(ELEL(MAX)) NAH() FUCK('[') FUCK(((int)((double)blush / (double)ELEL(MAX) * 1000)) / 10) FUCK("%]") NAH() DAFUCK(3) NAH() BRAIN(double, LEL, (((double)blush / (double)ELEL(MAX))*(double)ELEL(NICE))) SUMFUK2(FUCK("&"), (LEL)) SUMFUK2(NAH(), (ELEL(NICE) - LEL)) NAH() DAFUCK() FCKED(), ELEL(MAX)) COFFEE()
+ISFCKED()
